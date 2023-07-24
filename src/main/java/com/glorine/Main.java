67b54585767157daeaf4028935c014644951aef5
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2023-07-25 02:07:23 +0300

    new files added to project caesar cipher

---
+++ src/main/java/com/glorine/Main.java
@@ -0,0 +1,52 @@
+package com.glorine;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String operation;
+        String message;
+        int key;
+        UserInput userInput = new UserInput();
+        Checks checks = new Checks();
+        UserInput input;
+
+        System.out.println("Enter you operation **encode** or **decode**");
+        operation = scanner.nextLine();
+
+        System.out.println("Enter your message");
+        message = scanner.nextLine();
+
+        System.out.println("Enter key (0-25)");
+        key = scanner.nextInt();
+
+        userInput.setKey(key);
+        userInput.setMessage(message);
+        userInput.setOperation(operation);
+
+        input = userInput;
+
+        boolean userInputIsValid = checks.isValid(userInput);
+
+        if(userInputIsValid){
+            CaesarCipher caesarCipher = new CaesarCipher();
+            userInput = input;
+            if (userInput.getOperation().equalsIgnoreCase("encode")){
+                caesarCipher.encode(userInput.getMessage(), userInput.getKey());
+
+            }
+            else if (userInput.getOperation().equalsIgnoreCase("decode")){
+                caesarCipher.decode(userInput.getMessage(), userInput.getKey());
+            }
+        }
+        else{
+            System.out.println("Bad request, please try again");
+        }
+
+        scanner.close();
+    }
+
+}
