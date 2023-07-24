21cce9678b3c96027231a04fcbada984f0179131
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2023-07-25 02:39:46 +0300

    new packages added

---
+++ src/main/java/com/glorine/util/Checks.java
@@ -0,0 +1,27 @@
+package com.glorine.util;
+
+public class Checks {
+
+        public boolean isValid(UserInput userInput){
+            boolean operationIsValid = operationValidator(userInput.getOperation());
+            boolean messageIsValid = messageValidator(userInput.getMessage());
+            boolean keyIsValid = keyValidator(userInput.getKey());
+            return operationIsValid && messageIsValid && keyIsValid;
+        }
+        private boolean operationValidator(String operation){
+            boolean isValid = operation.equalsIgnoreCase("encode") || operation.equalsIgnoreCase("decode");
+            return isValid;
+        }
+        private boolean messageValidator(String message){
+            boolean isBlankorEmpty = message.isBlank() && message.isEmpty();
+            return !isBlankorEmpty;
+        }
+        private boolean keyValidator(int key){
+            if(key >=1 && key <= 25){
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
