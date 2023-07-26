39f3321480f1b187669b548ad2e81e9dbc71680e
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2023-07-27 02:53:29 +0300

    added tests for encoding and decoding method.

--- src/test/java/com/glorine/util/ChecksTest.java
+++ src/test/java/com/glorine/util/ChecksTest.java
@@ -8,12 +8,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class ChecksTest {
 
     @Test
-    void isValid() {
+    void testInvalidOperation_checkReturnsFalse() {
         Checks checks = new Checks();
         UserInput userInput = new UserInput();
         userInput.setOperation("rtrgrg");
         userInput.setMessage("message");
         userInput.setKey(1);
         assertFalse(checks.isValid(userInput));
+    }
+    @Test
+    public void testInvalidmessage_checkReturnsFalse() {
+        Checks checks = new Checks();
+        UserInput userInput = new UserInput();
+        userInput.setOperation("encode");
+        userInput.setMessage("");
+        userInput.setKey(1);
+        assertFalse(checks.isValid(userInput));
+    }
+    @Test
+    public void testInvalidkey_checkReturnsFalse() {
+        Checks checks = new Checks();
+        UserInput userInput = new UserInput();
+        userInput.setOperation("encode");
+        userInput.setMessage("hello");
+        userInput.setKey(26);
+        assertFalse(checks.isValid(userInput));
+    }
+    @Test
+    public void testValidInputs_checkReturnsTrue() {
+        Checks checks = new Checks();
+        UserInput userInput = new UserInput();
+        userInput.setOperation("encode");
+        userInput.setMessage("hello");
+        userInput.setKey(4);
+        assertTrue(checks.isValid(userInput));
     }
 }
